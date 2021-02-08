7ed5b90e471c1c10d8ed2691ded5d908d902a433
kylejschulz <kylschulz@gmail.com>
2021-02-08 13:07:03 -0500

    Finish song show page

--- spec/features/songs/show_spec.rb
+++ spec/features/songs/show_spec.rb
@@ -1,3 +1,24 @@
-As a visitor
-When I visit ‘/artists’
-I see a list of all artist names
+require 'rails_helper'
+
+RSpec.describe 'As a visitor' do
+  describe 'I visit a song show page' do
+    it "then I see only that song's title and artist" do
+      talking_heads = Artist.create!(name: 'Talking Heads')
+      she_was = talking_heads.songs.create(title: 'And She Was', length: 234, play_count: 2994 )
+      wild_life = talking_heads.songs.create(title: 'Wild Wild Life', length: 456, play_count: 384889 )
+
+      visit "/songs/#{she_was.id}"
+      save_and_open_page
+      
+
+      expect(page).to have_content(she_was.title)
+      expect(page).to have_content("By: #{talking_heads.name}")
+      expect(page).to_not have_content(wild_life.title)
+    end
+  end
+end
+
+
+# When I visit /songs/1 (where 1 is the id of a song in my database)
+# Then I see that song's title, and artist
+# And I do not see any of the other songs in my database
